4eb01380cbc6d7e14646b59459346829d934dc90
fareesthemass <hasanfarees7@gmail.com>
2024-06-29 12:35:53 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import britishcat from './assets/imgs/british shorthair.webp'
+import mainemoon from './assets/imgs/maine moon.webp'
+import persian from './assets/imgs/persian cat.webp'
+import siamese from './assets/imgs/siamese cat.webp'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    var catlist = [
+        {
+            img: britishcat,
+            p: "british Shorthair"
+        },
+        {
+            img: mainemoon,
+            p: "mainemoon cat"
+        },
+        {
+            img: persian,
+            p: 'persian cat'
+        },
+        {
+            img: siamese,
+            p: 'siamese cat'
+        },
+        {
+            img: britishcat,
+            p: "british Shorthair"
+        },
+        {
+            img: mainemoon,
+            p: "mainemoon cat"
+        },
+        {
+            img: siamese,
+            p: 'siamese cat'
+        },
+        {
+            img: persian,
+            p: 'persian cat'
+        },
+    ]
+
+    return (
+        
+        <div className="full-display">
+              <h1 style={{margin:'10px'}}>Image Gallery</h1>
+            <div className="full-container">
+          
+                {catlist.map((item) => {
+                    return (
+                        <div className="image-container">
+                            <img src={item.img} alt="" />
+                            <p>{item.p}</p>
+                        </div>)
+                })}
+
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
